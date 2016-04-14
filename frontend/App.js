50f9bfd9cdb356c2d4287abfccdb7f59b3d474ab
hhmpro <240050497@qq.com>
2016-04-14 11:39:35 +0800

    实现帖子列表和帖子详情页面

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import {Router, Route, Link, browserHistory} from 'react-router';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import TopicDetail from './component/TopicDetail';
+import TopicList from './component/TopicList';
+
+class Index extends React.Component {
+    render(){
+        return (
+            <div className="container">
+                <Header />
+                {this.props.children?this.props.children:<TopicList />}  
+                <Footer />
+            </div>
+        );
+    }
+}
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello----====-00====</h1>
+                <Router history={browserHistory}>
+                    <Route path="/" component={Index}>
+                        <Router path="topic/:id" component={TopicDetail} />
+                    </Route>
+                </Router>
             </div>
         );
     }
